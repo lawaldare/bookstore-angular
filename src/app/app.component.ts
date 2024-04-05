4c4e9aaa861e734fa64de6d6e20db08eb9bcfab0
Dare Lawal <lawaldare@gmail.com>
2024-04-05 20:39:10 +0200

    baba update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,37 @@
-import { BookService } from './services/book.service';
-import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { NgForm } from '@angular/forms';
-
-
+import { BookService } from "./services/book.service";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, NgForm } from "@angular/forms";
+import {
+  EMPTY,
+  catchError,
+  debounceTime,
+  filter,
+  map,
+  switchMap,
+  tap,
+} from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'bookstore';
-  searchedBook: string;
   searchedBookArray: any[];
-  loading = false;
-  information = 'Start searching for books';
+  information = "Start searching for books";
 
-  constructor(private bookService: BookService, private toastr: ToastrService) { }
+  public bookSearched = new FormControl("");
+
+  constructor(private bookService: BookService) {}
 
   ngOnInit() {
+    this.bookSearched.valueChanges
+      .pipe(
+        filter(Boolean),
+        debounceTime(300),
+        switchMap((term) => this.bookService.searchBooks(term)),
+        map((data) => (this.searchedBookArray = data.items))
+      )
+      .subscribe();
   }
-
-
-  searchBook(form: NgForm) {
-    this.loading = true;
-    this.searchedBookArray = [];
-    this.searchedBook = form.value.bookName;
-    this.bookService.searchBooks(this.searchedBook).subscribe(data => {
-    this.searchedBookArray = data.items;
-    if (this.searchedBookArray === undefined) {
-      this.information = 'Sorry, no results for your search';
-    }
-    this.loading = false;
-    form.resetForm();
-    }, error => {
-      this.toastr.error(error.message || error.error.message);
-    });
-    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BookComponent } from './components/book/book.component';
-import {FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BookComponent } from "./components/book/book.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BookComponent
-  ],
+  declarations: [AppComponent, BookComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
